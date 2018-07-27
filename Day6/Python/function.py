1f8646170d3e81a60b7dfe848c36b5ea2047bb88
Jeganathan Swaminathan <mail2jegan@gmail.com>
2018-07-27 14:45:19 +0530

    Added Python Function exercise.

---
+++ Day6/Python/function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def add(firstValue, secondValue):
+    return firstValue + secondValue
+
+x = 100
+y = 200
+
+print ("The result of " + str(x) + " + " + str(y) + " is " + str( add( 100, 200 ) ) )
+print ("The result of 100.5 + 200.9 is " , add( 100.5, 200.9 ) )
+print ("The result of Hello + World is " , add( 'Hello ', 'World' ) )
