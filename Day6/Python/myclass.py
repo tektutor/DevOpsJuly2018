bb3b00aa07b7446e71bcac1756d2a3e3044b321b
Jeganathan Swaminathan <mail2jegan@gmail.com>
2018-07-27 14:59:29 +0530

    Added class exercise.

---
+++ Day6/Python/myclass.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+class MyClass:
+
+    def __init__( self ):
+        print ( "MyClass constructor" )
+        self.x = 0
+        self.y = 0
+
+    def setValues ( self, firstValue, secondValue ):
+        self.x = firstValue
+        self.y = secondValue
+
+    def printValues ( self ):
+        print ('Value of x is ' + str ( self.x ) )
+        print ('Value of y is ' + str ( self.y ) )
+
+
+def main():
+
+    obj1 = MyClass()
+
+    obj1.printValues()
+    obj1.setValues ( 100, 200 )
+    obj1.printValues()
+
+    obj2 = MyClass()
+
+    obj2.printValues()
+    obj1.setValues ( 1000, 2000 )
+    obj1.printValues()
+
+main()
+
+
